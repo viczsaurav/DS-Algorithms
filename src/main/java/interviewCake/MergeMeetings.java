fd49bb8a03b0318a4f4ad4fc691db8306ed02de0
viczsaurav <viczsaurav@gmail.com>
2020-01-09 00:55:17 +0100

    Merge Meetings

--- src/main/java/interviewCake/MergeMeetings.java
+++ src/main/java/interviewCake/MergeMeetings.java
@@ -1,8 +1,61 @@
 package interviewCake;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+
+/**
+ * Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available.
+ *
+ * To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as an object of a Meeting class with integer variables startTime and endTime. These integers represent the number of 30-minute blocks past 9:00am.
+ *
+ *   public class Meeting {
+ *
+ *     private int startTime;
+ *     private int endTime;
+ *
+ *     public Meeting(int startTime, int endTime) {
+ *         // number of 30 min blocks past 9:00 am
+ *         this.startTime = startTime;
+ *         this.endTime   = endTime;
+ *     }
+ *
+ *     public int getStartTime() {
+ *         return startTime;
+ *     }
+ *
+ *     public void setStartTime(int startTime) {
+ *         this.startTime = startTime;
+ *     }
+ *
+ *     public int getEndTime() {
+ *         return endTime;
+ *     }
+ *
+ *     public void setEndTime(int endTime) {
+ *         this.endTime = endTime;
+ *     }
+ * }
+ * Java
+ * For example:
+ *
+ *   new Meeting(2, 3);  // meeting from 10:00 – 10:30 am
+ * new Meeting(6, 9);  // meeting from 12:00 – 1:30 pm
+ *
+ * Write a method mergeRanges() that takes a list of multiple meeting time ranges and returns a list of condensed ranges.
+ *
+ * For example, given:
+ *
+ *   [Meeting(0, 1), Meeting(3, 5), Meeting(4, 8), Meeting(10, 12), Meeting(9, 10)]
+ *
+ * your method would return:
+ *
+ *   [Meeting(0, 1), Meeting(3, 8), Meeting(9, 12)]
+ *
+ * Do not assume the meetings are in order. The meeting times are coming from multiple teams.
+ *
+ * Write a solution that's efficient even when we can't put a nice upper bound on the numbers representing our time ranges. Here we've simplified our times down to the number of 30-minute slots past 9:00 am. But we want the method to work even for very large numbers, like Unix timestamps. In any case, the spirit of the challenge is to merge meetings where startTime and endTime don't have an upper bound.
+ */
 
 public class MergeMeetings {
 
@@ -63,7 +116,9 @@ public class MergeMeetings {
 
 		// Step 1: Sort according to starting time
 		// in-place
-		meetings.sort((Meeting m1, Meeting m2)-> m1.startTime-m2.startTime);
+
+	  //meetings.sort((Meeting m1, Meeting m2)-> m1.startTime-m2.startTime);
+		meetings.sort(Comparator.comparingInt(Meeting::getStartTime));
 		Meeting[] sortedMeetings = meetings.toArray(new Meeting[meetings.size()]);
 
 		// Step 2: Merge if :

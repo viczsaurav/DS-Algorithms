2c8d5850bb58fe7d356d710f82dfaa20a8762b5d
viczsaurav <viczsaurav@gmail.com>
2020-01-22 23:20:13 +0100

    in-flight entertainment

--- src/main/java/interviewCake/InflightEntertainment.java
+++ src/main/java/interviewCake/InflightEntertainment.java
@@ -1,5 +1,8 @@
 package interviewCake;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InflightEntertainment {
 
 	/**
@@ -15,13 +18,26 @@ public class InflightEntertainment {
 	 * Don't make your users watch the same movie twice
 	 * Optimize for runtime over memory
 	 *
+	 *
+	 * Bonus
+	 * What if we wanted the movie lengths to sum to something close to the flight length (say, within 20 minutes)?
+	 * What if we wanted to fill the flight length as nicely as possible with any number of movies (not just 2)?
+	 * What if we knew that movieLengths was sorted? Could we save some space and/or time? => Binary search = logN
+	 *
 	 */
 
 	public static boolean canTwoMoviesFillFlight(int[] movieLengths, int flightLength) {
 
 		// determine if two movies add up to the flight length
 
+		Set<Integer> seenMovieSet = new HashSet<Integer>();
 
+		for(int m1: movieLengths){
+			int m2 = flightLength - m1;
+			if(seenMovieSet.contains(m2))
+				return true;
+			seenMovieSet.add(m1);
+		}
 		return false;
 	}
 

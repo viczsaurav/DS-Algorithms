c6a8b3f959bb76fdaca8a1619100464d148afdeb
viczsaurav <viczsaurav@gmail.com>
2020-01-26 09:54:00 +0100

    StockPrices-Greedy

---
+++ src/main/java/interviewCake/StockPrice.java
@@ -0,0 +1,56 @@
+package interviewCake;
+
+public class StockPrice {
+
+	/**
+	 * Writing programming interview questions hasn't made me rich yet ... so I might give up and start trading Apple stocks all day instead.
+	 *
+	 * First, I wanna know how much money I could have made yesterday if I'd been trading Apple stocks all day.
+	 *
+	 * So I grabbed Apple's stock prices from yesterday and put them in an array called stockPrices, where:
+	 *
+	 * The indices are the time (in minutes) past trade opening time, which was 9:30am local time.
+	 * The values are the price (in US dollars) of one share of Apple stock at that time.
+	 * So if the stock cost $500 at 10:30am, that means stockPrices[60] = 500.
+	 *
+	 * Write an efficient method that takes stockPrices and returns the best profit I could have made from one purchase and one sale of one share of Apple stock yesterday.
+	 *
+	 * For example:
+	 *
+	 *   int[] stockPrices = new int[] {10, 7, 5, 8, 11, 9};
+	 *
+	 * getMaxProfit(stockPrices);
+	 * // returns 6 (buying for $5 and selling for $11)
+	 *
+	 * No "shorting"—you need to buy before you can sell. Also, you can't buy and sell in the same time step—at least 1 minute has to pass.
+	 *
+	 */
+
+	public static int getMaxProfit(int[] stockPrices) {
+
+		// handled in test cases
+//		if (stockPrices.length < 2){
+//			throw new Exception("Atleast 2 prices required");
+//		}
+
+		// calculate the max profit
+		int min = stockPrices[0];
+		int max = stockPrices[1];
+		int maxProfit = max-min;
+
+		for (int i=1;i<stockPrices.length-1;i++){
+			if (min > stockPrices[i]){
+				min = stockPrices[i];
+				max = stockPrices[i+1];
+			} else {
+				// We can also just keep track of min/max for whole array, but always decreasing case is not handled then.
+				max = stockPrices[i+1];
+			}
+			if (maxProfit < (max-min))
+				maxProfit = (max-min);
+		}
+
+		return maxProfit;
+	}
+
+}
---
+++ src/test/java/interviewCake/TestStockPrice.java
@@ -0,0 +1,60 @@
+package interviewCake;
+
+import org.junit.jupiter.api.Test;
+
+import static interviewCake.StockPrice.getMaxProfit;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestStockPrice {
+	// tests
+
+	@Test
+	public void priceGoesUpThenDownTest() {
+		final int actual = getMaxProfit(new int[] {1, 5, 3, 2});
+		final int expected = 4;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void priceGoesDownThenUpTest() {
+		final int actual = getMaxProfit(new int[] {7, 2, 8, 9});
+		final int expected = 7;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void priceGoesUpAllDayTest() {
+		final int actual = getMaxProfit(new int[] {1, 6, 7, 9});
+		final int expected = 8;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void priceGoesDownAllDayTest() {
+		final int actual = getMaxProfit(new int[] {9, 7, 4, 1});
+		final int expected = -2;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void priceStaysTheSameAllDayTest() {
+		final int actual = getMaxProfit(new int[] {1, 1, 1, 1});
+		final int expected = 0;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void exceptionWithOnePriceTest(){
+		assertThrows(Exception.class, () -> {
+			getMaxProfit(new int[] {5});
+		});
+	}
+
+	@Test
+	public void exceptionWithEmptyPricesTest(){
+		assertThrows(Exception.class, ()-> {
+			getMaxProfit(new int[] {});
+		});
+	}
+}

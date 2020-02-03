30768b0b243820528cbbdd58b95a126362b0dfc7
viczsaurav <viczsaurav@gmail.com>
2020-02-03 22:37:32 +0100

    InPlaceArrayShuffle problem statement

--- src/main/java/interviewCake/InPlaceArrayShuffle.java
+++ src/main/java/interviewCake/InPlaceArrayShuffle.java
@@ -1,4 +1,12 @@
 package interviewCake;
 
 public class InPlaceArrayShuffle {
+
+	/**
+	 * The shuffle must be "uniform," meaning each item in the original array must have the
+	 * same probability of ending up in each spot in the final array.
+	 *
+	 * Assume that you have a method getRandom(floor, ceiling) for getting a random integer that is >= floor and <= ceiling.
+	 */
+
 }

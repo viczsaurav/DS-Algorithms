cfd731f1fc480008bb231470e3058b60e339e58e
viczsaurav <viczsaurav@gmail.com>
2020-02-13 13:51:44 +0100

    Balanced Binary Tree

--- src/main/java/interviewCake/BalancedBinaryTree.java
+++ src/main/java/interviewCake/BalancedBinaryTree.java
@@ -6,4 +6,34 @@ public class BalancedBinaryTree {
 	 *
 	 * A tree is "superbalanced" if the difference between the depths of any two leaf nodes is no greater than one.
 	 */
+
+	public static class BinaryTreeNode {
+
+		public int value;
+		public BinaryTreeNode left;
+		public BinaryTreeNode right;
+
+		public BinaryTreeNode(int value) {
+			this.value = value;
+		}
+
+		public BinaryTreeNode insertLeft(int leftValue) {
+			this.left = new BinaryTreeNode(leftValue);
+			return this.left;
+		}
+
+		public BinaryTreeNode insertRight(int rightValue) {
+			this.right = new BinaryTreeNode(rightValue);
+			return this.right;
+		}
+	}
+
+
+	public static boolean isBalanced(BinaryTreeNode treeRoot) {
+
+		// determine if the tree is superbalanced
+
+
+		return false;
+	}
 }

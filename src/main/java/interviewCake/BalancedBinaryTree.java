296cf0ab281104c8f4605ea9d517997c1f7d4a85
viczsaurav <viczsaurav@gmail.com>
2020-02-13 13:51:29 +0100

    Balanced Binary Tree

---
+++ src/main/java/interviewCake/BalancedBinaryTree.java
@@ -0,0 +1,9 @@
+package interviewCake;
+
+public class BalancedBinaryTree {
+	/**
+	 * Write a function to see if a binary tree ↴ is "superbalanced" (a new tree property we just made up).
+	 *
+	 * A tree is "superbalanced" if the difference between the depths of any two leaf nodes is no greater than one.
+	 */
+}
---
+++ src/test/java/interviewCake/TestBalancedBinaryTree.java
@@ -0,0 +1,88 @@
+package interviewCake;
+
+import interviewCake.BalancedBinaryTree.BinaryTreeNode;
+import static interviewCake.BalancedBinaryTree.isBalanced;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestBalancedBinaryTree {
+	// tests
+
+	@Test
+	public void fullTreeTest() {
+		final BinaryTreeNode root = new BinaryTreeNode(5);
+		final BinaryTreeNode a = root.insertLeft(8);
+		final BinaryTreeNode b = root.insertRight(6);
+		a.insertLeft(1);
+		a.insertRight(2);
+		b.insertLeft(3);
+		b.insertRight(4);
+		final boolean result = isBalanced(root);
+		assertTrue(result);
+	}
+
+	@Test
+	public void bothLeavesAtTheSameDepthTest() {
+		final BinaryTreeNode root = new BinaryTreeNode(3);
+		root.insertLeft(4).insertLeft(1);
+		root.insertRight(2).insertRight(9);
+		final boolean result = isBalanced(root);
+		assertTrue(result);
+	}
+
+	@Test
+	public void leafHeightsDifferByOneTest() {
+		final BinaryTreeNode root = new BinaryTreeNode(6);
+		root.insertLeft(1);
+		root.insertRight(0).insertRight(7);
+		final boolean result = isBalanced(root);
+		assertTrue(result);
+	}
+
+	@Test
+	public void leafHeightsDifferByTwoTest() {
+		final BinaryTreeNode root = new BinaryTreeNode(6);
+		root.insertLeft(1);
+		root.insertRight(0).insertRight(7).insertRight(8);
+		final boolean result = isBalanced(root);
+		assertFalse(result);
+	}
+
+	@Test
+	public void bothSubTreesSuperbalancedTest() {
+		final BinaryTreeNode root = new BinaryTreeNode(1);
+		root.insertLeft(5);
+		final BinaryTreeNode b = root.insertRight(9);
+		b.insertLeft(8).insertLeft(7);
+		b.insertRight(5);
+		final boolean result = isBalanced(root);
+		assertFalse(result);
+	}
+
+	@Test
+	public void bothSubTreesSuperbalancedTwoTest() {
+		final BinaryTreeNode root = new BinaryTreeNode(1);
+		final BinaryTreeNode a = root.insertLeft(2);
+		a.insertLeft(3);
+		a.insertRight(7).insertRight(8);
+		root.insertRight(4).insertRight(5).insertRight(6).insertRight(9);
+		final boolean result = isBalanced(root);
+		assertFalse(result);
+	}
+
+	@Test
+	public void onlyOneNodeTest() {
+		final BinaryTreeNode root = new BinaryTreeNode(1);
+		final boolean result = isBalanced(root);
+		assertTrue(result);
+	}
+
+	@Test
+	public void treeIsLinkedListTest() {
+		final BinaryTreeNode root = new BinaryTreeNode(1);
+		root.insertRight(2).insertRight(3).insertRight(4);
+		final boolean result = isBalanced(root);
+		assertTrue(result);
+	}
+}

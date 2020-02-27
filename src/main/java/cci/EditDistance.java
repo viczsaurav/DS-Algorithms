3d9dd6ec9b44ac8faa13a1b0d0c0c417b916f228
viczsaurav <viczsaurav@gmail.com>
2020-02-27 23:31:47 +0100

    livenstein Edit distance Problem

--- src/main/java/cci/EditDistance.java
+++ src/main/java/cci/EditDistance.java
@@ -1,12 +1,10 @@
 package cci;
 
-import java.util.Arrays;
-
 public class EditDistance {
 
 	/**
 	 *
-	 * Edit matrix will be like
+	 * Levenshtein Edit Distance matrix
 	 *
 	 * 	   ""	O R I G S T R
 	 * 	"" 0  1 2 3 4 5 6 7

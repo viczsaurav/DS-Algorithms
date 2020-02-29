9b59ee35bbe15c8c47707f7977b8f5de3fa4500e
viczsaurav <viczsaurav@gmail.com>
2020-03-01 00:33:42 +0100

    Topological Sort

--- src/main/java/searchingSortingDataStructures/TopologicalSortGraph.java
+++ src/main/java/searchingSortingDataStructures/TopologicalSortGraph.java
@@ -2,19 +2,19 @@ package searchingSortingDataStructures;
 
 import java.util.*;
 
-public class TopologicalSortGraph {
+public class TopologicalSortGraph<T> {
 
-	public void topologicalSort(Graphs graph){
+	public void topologicalSort(Graphs<T> graph){
 
 		// Base condition
 		if (graph.getGraphSize()==0){
 			throw new IllegalArgumentException("The input graph is empty");
 		}
 
-		Set<GraphVertex> visited = new HashSet<>();
-		Deque<GraphVertex> stack = new ArrayDeque<>();
+		Set<GraphVertex<T>> visited = new HashSet<>();
+		Deque<GraphVertex<T>> stack = new ArrayDeque<>();
 
-		for (GraphVertex vertex: graph.getAllVertices()){
+		for (GraphVertex<T> vertex: graph.getAllVertices()){
 			if(visited.contains(vertex)){
 				continue;
 			}
@@ -25,32 +25,32 @@ public class TopologicalSortGraph {
 
 	}
 
-	private void depthFirstSearch(Set<GraphVertex> visited, Deque<GraphVertex> stack, GraphVertex vertex){
+	private void depthFirstSearch(Set<GraphVertex<T>> visited, Deque<GraphVertex<T>> stack, GraphVertex<T> vertex){
 
 		// No need to break condition in recursion as function returned if last node,i.e no neighbours
 
 		// Update Visited here, since recursion
 		visited.add(vertex);
 
-		for (GraphVertex neighbour: vertex.getNeighbors()){
+		for (GraphVertex<T> neighbour: vertex.getNeighbors()){
 			if(visited.contains(neighbour))	continue;
 			depthFirstSearch(visited, stack, neighbour);
 		}
 		stack.offerFirst(vertex);
 	}
 
-	private void printStack(Deque<GraphVertex> stack){
+	private void printStack(Deque<GraphVertex<T>> stack){
 		while(!stack.isEmpty()){
 			System.out.println(stack.pollFirst().getLabel());
 		}
 	}
 
 	public static void main(String [] args){
-		final GraphVertex nodeA = new GraphVertex("A");
-		final GraphVertex nodeB = new GraphVertex("B");
-		final GraphVertex nodeC = new GraphVertex("C");
-		final GraphVertex nodeD = new GraphVertex("D");
-		final GraphVertex nodeE = new GraphVertex("E");
+		final GraphVertex<String> nodeA = new GraphVertex("A");
+		final GraphVertex<String> nodeB = new GraphVertex("B");
+		final GraphVertex<String> nodeC = new GraphVertex("C");
+		final GraphVertex<String> nodeD = new GraphVertex("D");
+		final GraphVertex<String> nodeE = new GraphVertex("E");
 		nodeA.addNeighbor(nodeB);
 		nodeA.addNeighbor(nodeC);
 		nodeB.addNeighbor(nodeC);
@@ -60,7 +60,7 @@ public class TopologicalSortGraph {
 		nodeC.addNeighbor(nodeE);
 		nodeE.addNeighbor(nodeD);
 
-		Graphs graph = new Graphs();
+		Graphs<String> graph = new Graphs();
 		graph.addVertex(nodeA);
 		graph.addVertex(nodeB);
 		graph.addVertex(nodeC);

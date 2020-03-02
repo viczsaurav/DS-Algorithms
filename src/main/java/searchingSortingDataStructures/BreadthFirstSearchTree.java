1ac8f25ca1dcc5309800e8ac0046920b71ecabf5
viczsaurav <viczsaurav@gmail.com>
2020-03-02 12:47:02 +0100

    Dynamic Programming

--- src/main/java/searchingSortingDataStructures/BreadthFirstSearchTree.java
+++ src/main/java/searchingSortingDataStructures/BreadthFirstSearchTree.java
@@ -1,5 +1,7 @@
 package searchingSortingDataStructures;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,6 +9,7 @@ public class BreadthFirstSearchTree {
 
     public static boolean breadthFirstSearch(Node root, int searchValue){
 
+        System.out.println("Searching for: "+ searchValue);
         Queue<Node> bft = new LinkedList<>();
         bft.add(root);
 
@@ -22,14 +25,37 @@ public class BreadthFirstSearchTree {
         return false;
     }
 
+    public static boolean breadthFirstSearchDeque(Node root, int searchValue){
+        System.out.println("Searching for: "+ searchValue);
+        Deque<Node> bft = new ArrayDeque<>();
+        bft.offerFirst(root);
+
+        while(!bft.isEmpty()){
+            Node currNode = bft.pollLast();
+            if(currNode.value== searchValue){
+                return true;
+            } else{
+                if(currNode.left!= null)    bft.offerFirst(currNode.left);
+                if(currNode.right!= null)   bft.offerFirst(currNode.right);
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[] intArray = {17, 11, 67, 3, 93, 23, 29, 97, 53};
         BinaryTree tree = new BinaryTree();
         tree.createBinaryTree(intArray);
 
         int searchValue1 = 23;
-        System.out.println(breadthFirstSearch(tree.getRootNode(), searchValue1));
         int searchValue2 = 21;
+
+        System.out.println(breadthFirstSearch(tree.getRootNode(), searchValue1));
         System.out.println(breadthFirstSearch(tree.getRootNode(), searchValue2));
+
+        System.out.println("----- Deque -----");
+        System.out.println(breadthFirstSearchDeque(tree.getRootNode(), searchValue1));
+        System.out.println(breadthFirstSearchDeque(tree.getRootNode(), searchValue2));
+
     }
 }

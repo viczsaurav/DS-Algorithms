0606dcb014152ddf4ff6718140bbd4254e4ebb35
viczsaurav <viczsaurav@gmail.com>
2020-03-09 23:42:02 +0100

    Trie

--- src/main/java/searchingSortingDataStructures/Trie.java
+++ src/main/java/searchingSortingDataStructures/Trie.java
@@ -5,10 +5,38 @@ import java.util.Map;
 
 public class Trie {
 
-	Map<Character, Trie> trieMap = new HashMap<>();
-	Boolean isLast;
+	private class TrieNode{
+		Map<Character, TrieNode> children;
+		Boolean endOfWord;
 
-	public Trie(Character c){
+		public TrieNode(){
+			this.children = new HashMap<>();
+			this.endOfWord = false;
+		}
+	}
+
+	private final TrieNode root;
+
+	public Trie(){
+		root = new TrieNode();
+	}
+
+	// Iterative Insert
+	public void insertNode(String word){
+			TrieNode current = root;
+			for(char ch: word.toCharArray()){
+				TrieNode node = current.children.get(ch);
+				if(node==null){
+					node = new TrieNode();
+					current.children.put(ch,node);
+				}
+				current=node;
+			}
+			// Word insert is complete, mark the endOfWord as true
+			current.endOfWord=true;
+	}
+
+	public void insertResursive(String word){
 
 	}
 }

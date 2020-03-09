88dd01dbbc056551b9d94d3b6688abc803daf71d
viczsaurav <viczsaurav@gmail.com>
2020-03-10 00:08:48 +0100

    Trie

--- src/main/java/searchingSortingDataStructures/Trie.java
+++ src/main/java/searchingSortingDataStructures/Trie.java
@@ -26,17 +26,62 @@ public class Trie {
 			TrieNode current = root;
 			for(char ch: word.toCharArray()){
 				TrieNode node = current.children.get(ch);
-				if(node==null){
-					node = new TrieNode();
-					current.children.put(ch,node);
-				}
+				node = node==null? new TrieNode(): node;
+				current.children.put(ch,node);
 				current=node;
 			}
 			// Word insert is complete, mark the endOfWord as true
 			current.endOfWord=true;
 	}
 
+	// Recursive Inserts
 	public void insertResursive(String word){
-
+		insertRecursive(root,word);
 	}
+
+	private void insertRecursive(TrieNode current, String word){
+		if(word.length()==0){
+			current.endOfWord=true;
+			return;
+		}
+
+		char ch = word.charAt(0);
+		TrieNode node = current.children.get(ch);
+		if (node == null) {
+			node = new TrieNode();
+			current.children.put(ch, node);
+		}
+
+		insertRecursive(node, word.substring(1));
+	}
+
+	// Iterative search
+	public boolean search(String word){
+		TrieNode current = root;
+
+		for (char ch: word.toCharArray()){
+			TrieNode node = current.children.get(ch);
+			if(node==null)
+				return false;
+			current= node;
+		}
+		return current.endOfWord;
+	}
+
+	 public boolean searchRecursive(String word){
+		return searchRecursive(root, word);
+	 }
+
+	 private boolean searchRecursive(TrieNode current, String word){
+		if(word.length()==0){
+			return current.endOfWord;
+		}
+		char ch = word.charAt(0);
+		TrieNode node =  current.children.get(ch);
+		if(node== null)
+			return false;
+
+		 return searchRecursive(node, word.substring(1));
+	 }
+
 }

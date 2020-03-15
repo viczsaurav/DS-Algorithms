ac51b7fcf565a006773be30f1ad2a904d2bd8707
viczsaurav <viczsaurav@gmail.com>
2020-03-15 15:24:47 +0100

    Anangram Finder with HashMap

---
+++ src/main/java/cci/AnagramFinderV3.java
@@ -0,0 +1,35 @@
+package cci;
+
+
+import java.util.*;
+
+public class AnagramFinderV3{
+
+	private static final HashMap<HashMap<Integer, Integer>,List<String>> dictionary = new HashMap<>();
+
+	public static void main(String []args){
+		System.out.println("Hello World");
+
+		List<String> dictList = Arrays.asList("cat","rat","mat","cinema","iceman","act","anemic","aaaaaa");
+
+		dictList.stream().forEach(word -> dictionary.compute(getIdentifierKey(word),
+						(k,v) -> (v==null)? new ArrayList<>():v)
+						.add(word));
+		dictionary.entrySet().forEach(e-> System.out.println(e.getKey()+"->"+e.getValue()));
+
+		HashMap<Integer, Integer> keyMap = getIdentifierKey("cat");
+		System.out.println("------------------------------------");
+		keyMap.entrySet().forEach(e-> System.out.println(e.getKey()+"->"+e.getValue()));
+
+		dictionary.get(keyMap).stream().forEach(System.out::println);
+
+	}
+
+	private static HashMap<Integer, Integer> getIdentifierKey(String word){
+		HashMap<Integer, Integer> keyMap = new HashMap<>();
+		for(int ch: word.toCharArray()){
+			keyMap.compute(ch, (k,v)-> v==null?1:v+1);
+		}
+		return keyMap;
+	}
+}

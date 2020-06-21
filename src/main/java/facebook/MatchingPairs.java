72389c7a67b35046da8cb97557ea363edd72dd7e
viczsaurav <viczsaurav@gmail.com>
2020-06-21 13:17:46 +0200

    Matching pairs

--- src/main/java/facebook/MatchingPairs.java
+++ src/main/java/facebook/MatchingPairs.java
@@ -2,6 +2,29 @@ package facebook;
 
 class MatchingPairs {
 
+	/**
+	 * https://www.facebookrecruiting.com/portal/coding_practice_question/?problem_id=559324704673058
+	 *
+	 * Given two strings s and t of length N, find the maximum number of possible matching pairs in strings s and t after swapping exactly two characters within s.
+	 * A swap is switching s[i] and s[j], where s[i] and s[j] denotes the character that is present at the ith and jth index of s, respectively. The matching pairs of the two strings are defined as the number of indices for which s[i] and t[i] are equal.
+	 *
+	 * Note: This means you must swap two characters at different indices.
+	 * Signature  :  int matchingPairs(String s, String t)
+	 * Input
+	 * 		s and t are strings of length N
+	 * 		N is between 2 and 1,000,000
+	 * Output
+	 * 		Return an integer denoting the maximum number of matching pairs
+	 *
+	 * Example 1
+	 * 		s = "abcd"
+	 * 		t = "adcb"
+	 * output = 4
+	 * Explanation:
+	 * 		Using 0-based indexing, and with i = 1 and j = 3, s[1] and s[3] can be swapped, making it  "adcb".
+	 *
+	 */
+
 	int matchingPairs(String s, String t) {
 		int count =0, i=0, j=0;
 

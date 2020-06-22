676187e2631c0205d198b173e4e6a0d7a4f0b5d3
viczsaurav <viczsaurav@gmail.com>
2020-06-22 12:26:39 +0200

    Revenue Milestones

--- src/main/java/facebook/RevenueMilestones.java
+++ src/main/java/facebook/RevenueMilestones.java
@@ -5,6 +5,15 @@ class RevenueMilestones {
  /**
   Time : KLog(N) Space :  [N]
 
+
+	https://www.facebookrecruiting.com/portal/coding_practice_question/?problem_id=192049171861831
+
+	Revenue Milestones
+	We keep track of the revenue Facebook makes every day, and we want to know on what days Facebook hits certain revenue milestones. Given an array of the revenue on each day, and an array of milestones Facebook wants to reach, return an array containing the days on which Facebook reached every milestone.
+	Signature : int[] getMilestoneDays(int[] revenues, int[] milestones)
+	Input : revenues is a length-N array representing how much revenue FB made on each day (from day 1 to day N).
+					milestones is a length-K array of total revenue milestones.
+	Output: Return a length-K array where K_i is the day on which FB first had milestones[i] total revenue.
  */
 
 

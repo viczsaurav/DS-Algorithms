a329f6439cd944a27b1c7afd1cb64b1bcc879336
viczsaurav <viczsaurav@gmail.com>
2020-06-29 10:02:03 +0200

    Reconstruct Itinerary

--- src/main/java/leetcode/GraphReconstructItinerary_P332.java
+++ src/main/java/leetcode/GraphReconstructItinerary_P332.java
@@ -2,6 +2,28 @@ package leetcode;
 
 import java.util.*;
 
+/**
+ * O(nlogn) time complexity from PriorityQueue (Time complexity should be O(n + nlogn + n) (build graph O(n + nlogn), Hierholzer O(n)))=>
+ * 		-> Inserting all the starting nodes, into the minHeap, would be in total O(nlogn) right, considering, for each insertion, take O(logn) time
+ *
+ * Also the recursive part would also have O(nlogn), since it would visit each node, and the estracting the minimum element from Heap and rebalancing it is in total a O(logn) operation.
+ *
+ *
+ * O(n) space complexity from recursion (Space complexity should be O(n), where n is the total number of tickets.)
+ *
+ *
+ *
+ *
+ *
+ * Approach:
+ *
+ * All the airports are vertices and tickets are directed edges. Then all these tickets form a directed graph.
+ * The graph must be Eulerian since we know that a Eulerian path exists.
+ * Thus, start from "JFK", we can apply the Hierholzer's algorithm to find a Eulerian path in the graph which is a valid reconstruction.
+ * Since the problem asks for lexical order smallest solution, we can put the neighbors in a min-heap. In this way, we always visit the smallest possible neighbor first in our trip.
+ *
+ *
+ */
 public class GraphReconstructItinerary_P332 {
 	private Map<String, PriorityQueue<String>> adj;
 	private List<String> out;

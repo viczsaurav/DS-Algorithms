ee809ba654b98270ea6e88374f8fa61f9f6c2494
viczsaurav <viczsaurav@gmail.com>
2020-06-30 12:18:23 +0200

    Bitwise Unique DeliveryId

--- src/main/java/interviewCake/BitwiseUniqueDeliveryId.java
+++ src/main/java/interviewCake/BitwiseUniqueDeliveryId.java
@@ -10,9 +10,6 @@ public class BitwiseUniqueDeliveryId {
 	public static int findUniqueDeliveryId(int[] deliveryIds) {
 		int result=0;
 
-		if (deliveryIds.length==0) return 0;
-		if (deliveryIds.length==1) return deliveryIds[0];
-
 		for (int i=0; i<deliveryIds.length;i++){
 			result = result^deliveryIds[i];
 		}

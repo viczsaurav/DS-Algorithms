2d89271f185533110dbf0497374e74c6f0e1703b
viczsaurav <viczsaurav@gmail.com>
2020-08-13 11:39:38 +0200

    House Robber - Dynamic programming

--- src/main/java/leetcode/HouseRobber_P198.java
+++ src/main/java/leetcode/HouseRobber_P198.java
@@ -14,10 +14,24 @@ public class HouseRobber_P198 {
      * https://leetcode.com/problems/house-robber/
      *
      *
+     * Example 1:
+     *
+     * Input: nums = [1,2,3,1]
+     * Output: 4
+     * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+     *              Total amount you can rob = 1 + 3 = 4.
+     *
+     * Example 2:
+     *
+     * Input: nums = [2,7,9,3,1]
+     * Output: 12
+     * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+     *              Total amount you can rob = 2 + 9 + 1 = 12.
+     *
      * @param nums
      * @return
      */
-    public int rob(int[] nums) {
+    public static int rob(int[] nums) {
         if(nums.length==0) return 0;
 
         int dp[] = new int[nums.length+1];
@@ -30,7 +44,14 @@ public class HouseRobber_P198 {
             // thus its not adjacent to the old max
             // e.g i=2, dp[3] = Math.max(dp[2], nums[2] + dp[1]);
         }
-
         return dp[nums.length];
+    }
+
+    public static void main(String[] args) {
+        int[] input1 = {1,2,3,1};
+        System.out.println("Expected: 4, Actual: "+ rob(input1));
+
+        int[] input2 = {2,7,9,3,1};
+        System.out.println("Expected: 12, Actual: "+ rob(input2));
     }
 }

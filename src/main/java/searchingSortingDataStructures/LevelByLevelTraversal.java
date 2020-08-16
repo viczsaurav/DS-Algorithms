83444751f60ffa64bbcb61d57e440ce8a736e117
viczsaurav <viczsaurav@gmail.com>
2020-08-16 21:46:15 +0200

    Queue BFS- Level By Level Traversal

--- src/main/java/searchingSortingDataStructures/LevelByLevelTraversal.java
+++ src/main/java/searchingSortingDataStructures/LevelByLevelTraversal.java
@@ -1,16 +1,20 @@
 package searchingSortingDataStructures;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class LevelByLevelTraversal {
 
+	/**
+	 * Using Null item as level breaker
+	 * @param root
+	 */
 	public static void levelByLevelTraversal(Node root){
-
 		Queue<Node> bft = new LinkedList<>();
 		bft.offer(root);
 		bft.offer(null);
-
 
 		while(!bft.isEmpty()){
 			Node currNode = bft.poll();
@@ -28,11 +32,39 @@ public class LevelByLevelTraversal {
 		}
 	}
 
+	/**
+	 * Using Deque with queue Size as level breaker
+	 * @param root
+	 */
+	public static void levelByLevelTraversalWithDeque(Node root){
+		Deque<Node> queue = new ArrayDeque<>();
+		queue.offerFirst(root);
+		int size=1;
+
+		while(!queue.isEmpty()){
+			Node node = queue.pollLast();
+			size--;
+
+			System.out.print(node.value+ "\t");
+			if(node.left!=null)	queue.offerFirst(node.left);
+			if(node.right!=null)	queue.offerFirst(node.right);
+
+			// Level break
+			if (size==0){
+				System.out.println();
+				size= queue.size();
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
 		int[] intArray = {17, 11, 67, 3, 93, 23, 29, 97, 53};
 		BinaryTree tree = new BinaryTree();
 		tree.createBinaryTree(intArray);
 
 		levelByLevelTraversal(tree.getRootNode());
+		System.out.println("\n----------------------");
+		levelByLevelTraversalWithDeque(tree.getRootNode());
 	}
 }

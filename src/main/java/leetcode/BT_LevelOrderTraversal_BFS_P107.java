1c702be90b3156da347949738d85f7fa911f7609
viczsaurav <viczsaurav@gmail.com>
2020-08-16 21:24:25 +0200

    Level Order Traversal - Queue BFS

---
+++ src/main/java/leetcode/BT_LevelOrderTraversal_BFS_P107.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.*;
+
+public class BT_LevelOrderTraversal_BFS_P107 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        List<List<Integer>> out = new ArrayList<>();
+        List<Integer> level = new ArrayList<>();
+
+        if (root == null) return out;
+
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        queue.offer(null);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                // Level Break
+                out.add(0, new ArrayList<>(level));
+                level = new ArrayList<>();
+
+                // queue empty means end of tree
+                if (!queue.isEmpty()) {
+                    queue.offer(null);
+                } else {
+                    level.add(node.val);
+                    if (node.left != null) queue.offer(node.left);
+                    if (node.right != null) queue.offer(node.right);
+                }
+            }
+        }
+        return out;
+    }
+}

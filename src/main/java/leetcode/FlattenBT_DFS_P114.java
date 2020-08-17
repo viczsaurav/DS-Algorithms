ee85ce2e4fcb5fe23ceb978577e2320e13a91597
viczsaurav <viczsaurav@gmail.com>
2020-08-17 17:14:04 +0200

    Flatten Binary tree to linkedList - DFS (recursive + stack)

---
+++ src/main/java/leetcode/FlattenBT_DFS_P114.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class FlattenBT_DFS_P114 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) { this.val = val;}
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+        The approach is to save right tree first and append the left tree to the right
+        and set left as null. And append saved right tree to the very bottom right
+        The left tree could be just one node or a list already. Hence after insert
+        into the right, we want to traverse to the very bottom right so that we
+        can append the early saved right, which could be a node or a list as well.
+    */
+    public static void flatten(TreeNode root) {
+        // Base case
+        if (root==null) return;
+
+        flatten(root.left);
+        flatten(root.right);    // this is postorder, inorder also works
+
+        TreeNode node = root.right; // save right tree
+        root.right = root.left;     // insert left tree to right
+        root.left = null;           // set left tree as null
+
+        while(root.right!=null) {   // set left tree as null
+            root= root.right;
+        }
+
+        root.right = node;
+    }
+
+    public void flattenWithStack(TreeNode root) {
+        if (root==null) return;
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+
+            // Post Order Traversal
+            if (node.right!=null)   stack.push(node.right);
+            if (node.left!=null)    stack.push(node.left);
+
+            // link the current node with the top node in the stack if exist
+            if (!stack.isEmpty()){
+                node.right = stack.peek();
+            }
+
+            node.left=null;
+        }
+    }
+}

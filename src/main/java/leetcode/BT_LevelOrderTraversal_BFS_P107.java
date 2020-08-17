d82f5eed3b3ad0c109c551f8b54af714cc975837
viczsaurav <viczsaurav@gmail.com>
2020-08-17 11:34:47 +0200

    Level Order Traversal from Bottom- Queue BFS

--- src/main/java/leetcode/BT_LevelOrderTraversal_BFS_P107.java
+++ src/main/java/leetcode/BT_LevelOrderTraversal_BFS_P107.java
@@ -16,15 +16,15 @@ public class BT_LevelOrderTraversal_BFS_P107 {
         }
     }
 
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+        // Dequeue can be used like levelByLevelTraversalWithDeque in file LevelByLevelTraversal using size concept
 
         List<List<Integer>> out = new ArrayList<>();
         List<Integer> level = new ArrayList<>();
 
         if (root == null) return out;
 
-        // For BFS, don't use Deque with `int` val TreeNode, as you cannot add null TreeNode to Deque.
-        // Dequeue can be used like levelByLevelTraversalWithDeque in file LevelByLevelTraversal using size concept
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         queue.offer(null);
@@ -33,19 +33,25 @@ public class BT_LevelOrderTraversal_BFS_P107 {
             TreeNode node = queue.poll();
             if (node == null) {
                 // Level Break
-                out.add(0, level);
-                level = new ArrayList<>();
+                out.add(0,level);
+                level=new ArrayList<>();
 
                 // queue empty means end of tree
                 if (!queue.isEmpty()) {
                     queue.offer(null);
-                } else {
-                    level.add(node.val);
-                    if (node.left != null) queue.offer(node.left);
-                    if (node.right != null) queue.offer(node.right);
                 }
+            } else {
+                level.add(node.val);
+                if (node.left != null)  queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
             }
         }
+
         return out;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        System.out.println(levelOrderBottom(root).toString());
     }
 }
